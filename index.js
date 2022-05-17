a28dcfd7a6cfda8901370067210cbc08dd4e4b9f
Brendan Ward <kingbeebop@gmail.com>
2022-05-17 10:17:34 -0400

    commit tree 410802cf01affd58a5b78fee824971ef6a08268e

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function countDown(n){
+    while (n >= 0){
+        console.log(n);
+        n -= 1;
+    }
+}
+
+function writeCards(names, type){
+    const thanks = [];
+    for (let i = 0; i < names.length; i++){
+        thanks.push(`Thank you, ${names[i]}, for the wonderful ${type} gift!`);
+    }
+    return thanks;
+}
